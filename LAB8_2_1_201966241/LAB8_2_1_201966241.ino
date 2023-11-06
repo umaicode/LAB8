929edd3d304ceced5f093f46240edb6c489a04a8
umaicode <park.seungchan99@gmail.com>
2023-11-06 19:52:16 +0900

    2-1. Hello World 표시

--- LAB8_2_1_201966241/LAB8_2_1_201966241.ino
+++ LAB8_2_1_201966241/LAB8_2_1_201966241.ino
@@ -2,6 +2,7 @@
 
 // 핀 번호 (RS, E, DB4, DB5, DB6, DB7)
 LiquidCrystal lcd(44, 45, 46, 47, 48, 49);  // LCD 연결
+
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2); // LCD 초기화
